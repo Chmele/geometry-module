60f0589f50e4a5d17551bd608c62f482e6af3d39
artandfi <artyom.fisunenko@gmail.com>
2021-02-21 19:12:27 +0000

    add graph class

---
+++ graph.py
@@ -0,0 +1,18 @@
+from point import Point
+from vertex import Vertex
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = []
+        self.edges = []
+    
+    def __str__(self):
+        return str(self.edges)
+
+    def add_vertex(self, v: Vertex):
+        self.vertices.append(v)
+
+    def add_edge(self, v1: Vertex, v2: Vertex, weight = 0):
+        if (v1 in self.vertices and v2 in self.vertices):
+            self.edges.append((v1.id, v2.id, weight))
--- vertex.py
+++ vertex.py
@@ -1,12 +1,10 @@
-from .point import Point
+from point import Point
 
 
 class Vertex:
-    def __init__(self, point: Point):
+    def __init__(self, point: Point, id):
         self.point = point
-        self.id = 0
-        self.adjacent = {}
+        self.id = id
     
     def __str__(self):
-        return 'Vertex #' + str(self.id) + ', adjacent: ' + \
-            str([x.id for x in self.adjacent])
+        return 'Vertex #' + str(self.id)
