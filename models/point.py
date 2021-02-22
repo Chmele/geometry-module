fb0b44165239a71ff4e4f10548cca1d787138edb
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2021-02-22 15:44:55 +0200

    point class n-dimensional

--- models/point.py
+++ models/point.py
@@ -1,7 +1,26 @@
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
+
+    def __init__(self, *args):
+        self.coords = tuple(map(lambda x: float(x), args))
+
+    @property
+    def x(self):
+        return self.coords[0]
+
+    @property
+    def y(self):
+        return self.coords[1]
+
+    @property
+    def z(self):
+        return self.coords[2]
+
+    @property
+    def dim(self):
+        return len(self.coords)
+
     def __str__(self):
-        return "(%s,%s)" % (self.x, self.y)
+        return "(%s, %s)" % (self.x, self.y)
+
+    def __le__(self, other):
+        return self.coords < other.coords
--- models/vertex.py
+++ models/vertex.py
@@ -1,3 +1,6 @@
+from models import Point
+
+
 class Vertex:
     def __init__(self, point):
         self.point = point
