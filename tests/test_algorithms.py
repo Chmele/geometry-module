7f5b00c7351e043082efefb72c55cde4d2b94191
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2021-03-01 13:28:37 +0200

    test provide for stripe method

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,6 +1,8 @@
 import unittest
-from models import Point, Vertex, Graph
+from models import Point, Vertex, Graph, Edge
 from algo import stripe_method as s
+import math
+import functools as f
 
 
 class TestAlgorithms(unittest.TestCase):
@@ -38,5 +40,30 @@ class TestAlgorithms(unittest.TestCase):
         dot = Point(11.5, 5.5)
 
         ans = list(s.stripe(g, dot))
-        # print(str(ans[1][(5.0, 7.0)][4]))
-        print(str(ans[3][1]))
+        self.assertEqual(ans[0], [(-math.inf, 0.0), (0.0, 2.5), (2.5, 3.0), (3.0, 5.0), (5.0, 7.0), (7.0, 8.0), (8.0, 11.0), (11.0, math.inf)])
+       
+        self.assertTrue(self.fragmentation_eq(ans[1], 
+        {
+            (-math.inf, 0.0): [], 
+            (0.0, 2.5): [Edge(p1, p2), Edge(p1, p3)], 
+            (2.5, 3.0): [Edge(p1, p3), Edge(p2, p3), Edge(p2, p5)], 
+            (3.0, 5.0): [Edge(p2, p5), Edge(p3, p6)], 
+            (5.0, 7.0): [Edge(p2, p5), Edge(p4, p5), Edge(p4, p7), Edge(p4, p6), Edge(p3, p6)], 
+            (7.0, 8.0): [Edge(p5, p7), Edge(p4, p7), Edge(p4, p6), Edge(p3, p6)], 
+            (8.0, 11.0): [Edge(p5, p7), Edge(p4, p7), Edge(p7, p6)], 
+            (11.0, math.inf): []
+        }))
+
+        self.assertEqual(ans[2], (5.0, 7.0))
+        self.assertEqual(ans[3], [Edge(p4, p6), Edge(p3, p6)])
+
+    def fragmentation_eq(self, f1, f2):
+        for i in f1:
+            for item in f1[i]:
+                if item not in f2[i]:
+                    return False
+        for i in f2:
+            for item in f2[i]:
+                if item not in f1[i]:
+                    return False
+        return True
