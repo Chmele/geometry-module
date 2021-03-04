dff1721aef2e77460abcebfbf94f444b1ff71a22
MaksymRud <mr.freiermann01@gmail.com>
2021-03-04 19:02:51 +0200

    change init package

--- algo/__init__.py
+++ algo/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 from algo import stripe_method
 from algo import kd_tree_method
 from algo import chaine_method
-=======
-from algo import stripe_method
-from algo import kd_tree_method
->>>>>>> main
