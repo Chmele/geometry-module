714e96bbc60a5cab110c18036d7868e21b397969
artandfi <artyom.fisunenko@gmail.com>
2021-03-15 15:35:16 +0200

    update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from models.edge import Edge
 from models.graph import Graph
 from models.bin_tree_node import Node
 from models.bin_tree import BinTree
+from models.kd_tree import KdTree
+from models.line2d import Line2D
