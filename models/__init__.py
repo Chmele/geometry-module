9403cf38a6d20466694117e93869a614ff8196ba
MaksymRud <mr.freiermann01@gmail.com>
2021-03-21 00:07:56 +0200

    changing import

--- models/__init__.py
+++ models/__init__.py
@@ -4,10 +4,6 @@ from models.edge import Edge, OrientedEdge
 from models.graph import Graph, OrientedGraph
 from models.bin_tree_node import Node, NodeWithParent
 from models.bin_tree import BinTree, BinTreeChains
-from models.edge import Edge
-from models.graph import Graph
-from models.bin_tree_node import Node
-from models.bin_tree import BinTree
 from models.vector import Vector
 from models.polygon import Polygon
 from models.kd_tree import KdTree
