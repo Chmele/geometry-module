5e6d83c081d5b4024a50dfd5154e013ae75b7870
MaksymRud <mr.freiermann01@gmail.com>
2021-03-21 00:13:21 +0200

    adding equality for a vertex. It is for unittests

--- models/vertex.py
+++ models/vertex.py
@@ -2,6 +2,9 @@ class Vertex:
     def __init__(self, point):
         self.point = point
 
+    def __eq__(self, other):
+        return self.point.__eq__(other)
+
     def __getitem__(self, x):
         return self.point.coords[x]
 
