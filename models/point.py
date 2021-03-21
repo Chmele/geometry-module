410a74503c9b231dd9b75c4c6cf74cfcd7c05511
artandfi <artyom.fisunenko@gmail.com>
2021-03-21 15:28:36 +0200

    provide docstrings for point class methods

--- models/point.py
+++ models/point.py
@@ -9,7 +9,7 @@ class Point:
         self.coords = tuple(map(lambda x: float(x), args))
 
     def dominating(self, other):
-        """True if each self coordinate is bigger than other"""
+        '''True if each self coordinate is bigger than other'''
         return reduce(
             lambda a, b: a and b[0] >= b[1], zip(self.coords, other.coords), True
         )
@@ -30,10 +30,6 @@ class Point:
     def dim(self):
         return len(self.coords)
 
-    @property
-    def norm(self):
-        return math.sqrt(sum(x ** 2 for x in self.coords))
-
     def __getitem__(self, key):
         return self.coords[key]
 
@@ -53,25 +49,29 @@ class Point:
         return Point(*list(map(sub, self.coords, other.coords)))
 
     def dist_to_point(self, other):
+        '''Euclidean distance to point'''
         s = sum([(a - b) ** 2 for a, b in zip(self.coords, other.coords)])
         return math.sqrt(s)
 
     def dist_to_line(self, line):
-        return abs(line.A * self.x + line.B * self.y + line.C) / math.sqrt(
-            line.A ** 2 + line.B ** 2
+        '''Euclidean distance to the 2D line'''
+        return (
+            abs(line.A * self.x + line.B * self.y + line.C) /
+            math.sqrt(line.A ** 2 + line.B ** 2)
         )
 
-    def polar_angle_with(self, other):
-        return math.atan2(self.y - other.y, self.x - other.x)
-
     def angle_with(self, point1, point2):
-        """Angle point1 - self - point2 in [-pi, pi]"""
+        '''Angle point1-self-point2 in [-pi, pi]'''
         v1 = Vector.from_two_points(self, point1)
         v2 = Vector.from_two_points(self, point2)
         v1.normalize()
         v2.normalize()
 
         return math.acos(v1 * v2 / (v1.euclidean_norm * v2.euclidean_norm))
+
+    def polar_angle_with(self, other):
+        '''Polar angle between self and other with self as origin'''
+        return math.atan2(self.y - other.y, self.x - other.x)
 
     def __hash__(self):
         return hash(self.coords)
@@ -92,5 +92,5 @@ class Point:
 
     @staticmethod
     def centroid(point_iter):
-        """returns mean of points iterable"""
+        '''Coordinate-wise mean of points iterable'''
         return Point(*(sum(coord) / len(coord) for coord in zip(*point_iter)))
