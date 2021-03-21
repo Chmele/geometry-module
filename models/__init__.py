7075879e7a7c36502675deea6a2e1070b763b77b
MaksymRud <mr.freiermann01@gmail.com>
2021-03-21 23:10:26 +0200

    correcting imports

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,6 @@ from models.edge import Edge, OrientedEdge
 from models.graph import Graph, OrientedGraph
 from models.bin_tree_node import Node, NodeWithParent
 from models.bin_tree import BinTree, BinTreeChains
-from models.vector import Vector
-from models.polygon import Polygon
 from models.kd_tree import KdTree
 from models.line2d import Line2D
 from models.triangle import Triangle
