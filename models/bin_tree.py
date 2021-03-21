f69c58e7fe7f945f28a83248f92bc9dd034a220d
MaksymRud <mr.freiermann01@gmail.com>
2021-03-21 14:23:26 +0200

    fix find_dot

--- models/bin_tree.py
+++ models/bin_tree.py
@@ -1,4 +1,4 @@
-from models import Node, Point
+from models import Node, Point, NodeWithParent, Edge
 from typing import Tuple, List
 
 class BinTree:
@@ -20,8 +20,8 @@ class BinTree:
             self.print_tree(root.right)
 
 class BinTreeChains(BinTree):
-    def __init__(self, root: Node, x_range, y_range):
-        super().__init__(root, x_range, y_range)
+    def __init__(self, root: Node):
+        super().__init__(root)
     
     def make_tree(self, list: List, node: Node):
         mid = len(list) // 2
@@ -33,31 +33,41 @@ class BinTreeChains(BinTree):
         list_r = list[-mid:]
         left, right = list_l[mid // 2], list_r[mid // 2]
 
-        node.left = Node(left, node)
+        node.left = NodeWithParent(left, node)
         if (node.data != right):
-            node.right = Node(right, node)
+            node.right = NodeWithParent(right, node)
 
         self.make_tree(list_l, node.left)
         self.make_tree(list_r, node.right)
 
+    def _point_in_edge(self, edge: Edge, point):
+        if (edge.v1[1] <= point.y and edge.v2[1] >= point.y):
+            return True
+        else:
+            return False
+
     def search_dot(self, point: Point) -> Tuple:
         current_node = self.root 
-        while not (current_node.left == None and current_node.right == None):
-            edge = list(filter(lambda edge: edge.v1.point.y >= point.y and edge.v2.point.y < point.y, current_node.data))[0]
+        location = 0
+        right_parent = None
+        left_parent = None
+
+        while current_node is not None:
+            edge = list(filter(lambda edge: self._point_in_edge(edge, point), current_node.data))[0]
             location = (point.x - edge.v1.point.x)*(edge.v2.point.y - edge.v1.point.y) - (point.y - edge.v1.point.y)*(edge.v2.point.x - edge.v1.point.x)
+
             if location > 0:
-                
-                if current_node.right:
+                if current_node.right is not None:
                     current_node = current_node.right
                     left_parent = current_node.parent
-                else:
-                    return (left_parent, current_node)
+                else:                    
+                    return (current_node.data, right_parent.data)
+            
             elif location < 0:
-                
-                if current_node.left:
+                if current_node.left is not None:
                     current_node = current_node.left
                     right_parent = current_node.parent
                 else:
-                    return (current_node, right_parent)
+                    return (left_parent.data, current_node.data)
             else:
-                return(current_node, None)
+                return(current_node.data, None)
