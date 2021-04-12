9eb02f900fed04dba5899d8efc6270ece8631575
artandfi <artyom.fisunenko@gmail.com>
2021-04-12 22:07:03 +0300

    fix tests for triangle

--- models/triangle.py
+++ models/triangle.py
@@ -1,55 +1,21 @@
 import math
-from operator import itemgetter
-from models import Point
 
 
 class Triangle:
-    def __init__(self, p1: Point, p2: Point, p3: Point):
-        self.points = (p1, p2, p3)
-
-    @property
-    def a(self):
-        return self.points[0]
-
-    @property
-    def b(self):
-        return self.points[1]
-
-    @property
-    def c(self):
-        return self.points[2]
+    def __init__(self, A, B, C):
+        """Make triangle from 3 objects."""
+        self.A, self.B, self.C = A, B, C
 
     @property
     def sides(self):
-        ig = itemgetter(2, 3, 1)
         return (
             p1.dist_to_point(p2)
-            for p1, p2 in zip(self.points, ig(self.points))
+            for p1, p2 in zip((self.C, self.A, self.B), (self.B, self.C, self.A))
         )
 
     @property
     def area(self):
         """Heron`s formula."""
         p = sum(self.sides) / 2
-        ab, bc, ca = self.sides
-        return math.sqrt(p * (p - ab) * (p - bc) * (p - ca))
-
-    def point_belonging(self, p: Point):
-        """
-        Check if given point belongs to triangle
-        :param p: point to check
-        :return: True if belongs, False otherwise
-        """
-        a = (self.a.x - p.x) * (self.b.y - self.a.y) - (self.b.x - self.a.x) * (self.a.y - p.y)
-        b = (self.b.x - p.x) * (self.c.y - self.b.y) - (self.c.x - self.b.x) * (self.b.y - p.y)
-        c = (self.c.x - p.x) * (self.a.y - self.c.y) - (self.a.x - self.c.x) * (self.c.y - p.y)
-        return ((a >= 0) and (b >= 0) and (c >= 0)) or ((a <= 0) and (b <= 0) and (c <= 0))
-
-    def __eq__(self, other):
-        return set(self.points) == set(other.points)
-
-    def __hash__(self):
-        return hash(self.points)
-
-    def __str__(self):
-        return str(self.points)
+        A, B, C = self.sides
+        return math.sqrt(p * (p - A) * (p - B) * (p - C))
